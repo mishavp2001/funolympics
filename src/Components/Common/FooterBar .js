d9a120af1284268014cae8481b15f84f42cab887
mishavp2001 <mvp@Michaels-MacBook-Pro.local>
2023-10-24 12:19:32 -0700

    Initia, vcommit

---
+++ src/Components/Common/FooterBar .js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../../App.css'
+
+const FooterBar = props => {
+    return (
+        <div>
+            <h6>Footer</h6>
+        </div>
+    );
+};
+
+FooterBar.propTypes = {
+    
+};
+
+export default FooterBar;
---
+++ src/Components/Common/NavigationBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../../App.css'
+
+const NavigationBar = props => {
+    return (
+        <div>
+            <h6>Navigation</h6>
+        </div>
+    );
+};
+
+NavigationBar.propTypes = {
+    
+};
+
+export default NavigationBar;
